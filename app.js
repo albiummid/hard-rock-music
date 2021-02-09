1da11bb80307f218f711dca237f6d3d22ea6caed
Albi Ummid <albi.ummid@gmail.com>
2021-02-09 14:30:14 +0600

    without async

---
+++ app.js
@@ -0,0 +1,42 @@
+const searchSong = () =>{
+    const searchText = document.getElementById('search-field').value;
+    fetch(`https://api.lyrics.ovh/suggest/${searchText}`)
+    // load data
+    .then(res => res.json())
+    .then(data => displaySongs(data.data))
+
+}
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+
+    songs.forEach(song => {
+        const songDiv = document.createElement("div");
+        songDiv.className = "single-result row align-items-center my-3 p-3"
+         songDiv.innerHTML = `
+          <div class="col-md-9">
+          <img src="${song.album.cover_medium}" class = "w-25">
+         <h3 class="lyrics-name">${song.title}</h3>
+         <p class="author lead">Album by <span>${song.artist.name}</span></p>
+         <audio controls>
+  <source src="${song.preview}" type="audio/mpeg">
+</audio>
+     </div>
+     <div class="col-md-3 text-md-right text-center">
+         <button onclick = "getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+     </div>`
+         songContainer.appendChild(songDiv);
+    });
+
+}
+
+const getLyric = (artist, title) => {
+fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+.then(res => res.json())
+.then(data => displayLyrics(data.lyrics))
+}
+
+const displayLyrics = lyrics => {
+    const lyricDiv = document.getElementById("song-lyrics");
+    lyricDiv.innerText = lyrics;
+}
